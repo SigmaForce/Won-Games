bc815deb05593500cba156d06f93702670448e80
Leonardo Lucas <leonardolucas2020@gmail.com>
2021-01-17 21:54:43 -0400

    Adicionadno Storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,10 @@
+import * as S from './styles'
 const Main = () => (
-    <main>
-        <h1>React Avançado</h1>
-    </main>
+    <S.Wrapper>
+        <S.Logo src="/img/logo.svg" alt="Imagem de um Atómo e React Avançado escrito ao lado"/>
+        <S.Title>React Avançado</S.Title>
+        <S.Description>TypeScript, ReactJS, NextJS, Styled Components</S.Description>
+        <S.Ilustration src="/img/hero-illustration.svg" alt="Um desenvolvedor de frente com a tela com código"></S.Ilustration>
+    </S.Wrapper>
 )
 export default Main
---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import {AppProps} from 'next/app'
+import Head from 'next/head'
+import GlobalStyles from 'styles/global'
+function App({ Component, pageProps }:AppProps ){
+  return (<>
+          <Head>
+            <title>React-Avançado - boilerplate</title>
+            <link rel="shortcut icon" href="/img/icon-512.png"/>
+            <link rel="apple-touch-icon" href="/img/icon-512.png"/>
+            <meta name="description" content="A Simple project work with TypeScript, React, NextJS And Styled Components"/>
+          </Head>
+          <GlobalStyles/>
+            <Component {...pageProps} />
+          </>)
+}
+
+export default App
