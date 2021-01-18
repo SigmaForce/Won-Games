214e3a3180e032f4a502c2834cfb6711d46f4871
Leonardo Lucas <leonardolucas2020@gmail.com>
2021-01-17 22:37:07 -0400

    Configurando storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
-
+import {addDecorator} from '@storybook/react'
+import withGlobalStyles from './withGLobalStyles'
+addDecorator(withGlobalStyles)
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,12 @@
+import {withKnobs,text} from '@storybook/addon-knobs'
+import Main from '.'
+
+export default {
+    title: 'Main',
+    component: Main,
+    decorator:[withKnobs]
+}
+export const Basic = () => 
+<Main 
+title={text('Title','React Avançado')}
+description={text('Description','TypeScript, ReactJS, NextJS, Styled Components') }/>
